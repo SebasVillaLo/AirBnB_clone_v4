b9ba5374fc16163b6d83641c68fa5f31f1386f27
afecego <afecego@gmail.com>
2022-02-10 13:15:59 -0500

    Exercice 2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,11 +12,14 @@ $(function () {
 });
 
 const web = 'http://0.0.0.0:5001/api/v1/status/';
-$.get(web, function (data, statusText, xhr) {
-  if (xhr.status === 200) {
-    $("div#api_status").addClass("available");
-    return;
-  } else {
-    $("div#api_status").removeClass("available");
-  }
+$(function () {
+  $.get(web, function (data, statusText, xhr) {
+    if (xhr.status === 200) {
+      alert("malo")
+      $("div#api_status").addClass("available");
+      return;
+    } else {
+      $("div#api_status").removeClass("available");
+    }
+  })
 });
