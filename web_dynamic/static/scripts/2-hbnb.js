bab4458c3d81bea9ca5a4a5ee3ea78afa8fd347f
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-10 16:31:25 -0500

    add api and task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -7,19 +7,24 @@ $(function () {
       const index = mylist.indexOf($(this).attr('data-name'));
       mylist.splice(index, 1);
     }
-    $('.amenities h4').text(mylist.join(', '));
-  });
-});
+    function cortarTextoConPuntos(texto, limite) {
+      var puntosSuspensivos = "...";
+      if (texto.length > limite) {
+        texto = texto.substring(0, limite) + puntosSuspensivos;
+      }
 
-const web = 'http://0.0.0.0:5001/api/v1/status/';
-$(function () {
-  $.get(web, function (data, statusText, xhr) {
-    if (xhr.status === 200) {
-      alert("malo")
-      $("div#api_status").addClass("available");
-      return;
+      return texto;
+    }
+
+    $('.amenities h4').text(cortarTextoConPuntos(mylist.join(', '), 42));
+    $('div .amenities h4').css("width", "300px");
+    $('div .amenities h4').css("height", "16.5px");
+  });
+  $.getJSON(`http://${window.location.hostname}:5001/api/v1/status/`, function (data) {
+    if (data.status === "OK") {
+      $("#api_status").addClass("available");
     } else {
-      $("div#api_status").removeClass("available");
+      $("#api_status").removeClass("available");
     }
   })
 });
