c161751cd14677bd32d57da7bc369878e59bbb07
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-10 14:50:05 -0500

    fix text desbording

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,23 @@
-$(function() {
+$(function () {
   var mylist = [];
-  $('input[type="checkbox"]').on( 'change', function() {
-    if( $(this).is(':checked') ) {
+  $('input[type="checkbox"]').on('change', function () {
+    if ($(this).is(':checked')) {
       mylist.push($(this).attr('data-name'));
     } else {
       const index = mylist.indexOf($(this).attr('data-name'));
       mylist.splice(index, 1);
     }
-    $('.amenities h4').text(mylist.join(', '));
+    function cortarTextoConPuntos(texto, limite) {
+      var puntosSuspensivos = "...";
+      if (texto.length > limite) {
+        texto = texto.substring(0, limite) + puntosSuspensivos;
+      }
+
+      return texto;
+    }
+
+    $('.amenities h4').text(cortarTextoConPuntos(mylist.join(', '), 42));
+    $('div .amenities h4').css("width", "300px");
+    $('div .amenities h4').css("height", "16.5px");
   });
 });
