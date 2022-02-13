3e8e28910c5bd001c7d23a4e25c40e2843d7d9b6
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-13 01:08:51 -0500

    UPD: refactoring of the functions

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,24 @@
 $(function () {
-  var mylist = {};
-  function checkBox() {
-    $('input[type="checkbox"]').on('change', function () {
-      if ($(this).is(':checked')) {
-        mylist[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete mylist[$(this).attr('data-id')]
-      }
-      function cortarTextoConPuntos(texto, limite) {
-        var puntosSuspensivos = "...";
-        if (texto.length > limite) {
-          texto = texto.substring(0, limite) + puntosSuspensivos;
-        }
-
-        return texto;
-      }
-
-      $('.amenities h4').text(cortarTextoConPuntos(Object.values(mylist).join(', '), 42));
-    });
-  }
   checkBox();
 });
+
+var mylist = {};
+function checkBox() {
+  $('input[type="checkbox"]').on('change', function () {
+    if ($(this).is(':checked')) {
+      mylist[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete mylist[$(this).attr('data-id')]
+    }
+    function cortarTextoConPuntos(texto, limite) {
+      var puntosSuspensivos = "...";
+      if (texto.length > limite) {
+        texto = texto.substring(0, limite) + puntosSuspensivos;
+      }
+
+      return texto;
+    }
+
+    $('.amenities h4').text(cortarTextoConPuntos(Object.values(mylist).join(', '), 42));
+  });
+}
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,11 @@
 $(function () {
-  var mylist = {};
-  function checkBox () {
-    $('input[type="checkbox"]').on('change', function () {
+  checkBox();
+  getStatus();
+});
+
+var mylist = {};
+function checkBox() {
+  $('input[type="checkbox"]').on('change', function () {
     if ($(this).is(':checked')) {
       mylist[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
@@ -19,16 +23,12 @@ $(function () {
     $('.amenities h4').text(cortarTextoConPuntos(Object.values(mylist).join(', '), 42));
   });
 }
-  function getStatus () {
-    $.getJSON(`http://${window.location.hostname}:5001/api/v1/status/`, function (data) {
-      if (data.status === "OK") {
-        $("#api_status").addClass("available");
-      } else {
-        $("#api_status").removeClass("available");
-      }
-    })
-  }
-  checkBox();
-  getStatus();
-});
-
+function getStatus() {
+  $.getJSON(`http://${window.location.hostname}:5001/api/v1/status/`, function (data) {
+    if (data.status === "OK") {
+      $("#api_status").addClass("available");
+    } else {
+      $("#api_status").removeClass("available");
+    }
+  })
+}
